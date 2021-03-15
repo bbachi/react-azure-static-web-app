656f2cfa4e113955143ee48f5685f6cce5016c6a
Bhargav Bachina <jcnt845@MACX-00138.local>
2021-03-15 00:16:30 -0500

    added settings

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header'
